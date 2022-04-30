15b4d25c98253c3d60166a75908959fcd60cb364
Kazuko Baynton <kazukobaynton@gmail.com>
2022-04-29 17:55:40 -0700

    showing seller_name instad of seller_id, add more query in items.js to retrieve seller name

--- routes/items.js
+++ routes/items.js
@@ -5,7 +5,11 @@ module.exports = (db) => {
 
   // show a specific item
   router.get("/:id", (req, res) => {
-    let queryString = `SELECT * FROM items where id = $1`;
+    let queryString = `
+    SELECT items.*, users.name AS seller_name
+    FROM items
+    JOIN users ON seller_id=users.id
+    WHERE items.id = $1;`;
 
     db.query(queryString,[req.params.id])
       .then(data => {
