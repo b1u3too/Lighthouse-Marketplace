7ad83dfcd55fd3c961406a8e9c4d6a35baed10d6
Ziyu Huang <aprilhuang825@gmail.com>
2022-04-30 00:58:06 +0000

    create add new listing page and route

--- routes/myListings.js
+++ routes/myListings.js
@@ -24,5 +24,31 @@ module.exports = (db) => {
       });
   });
 
+  // go to add new listing page
+  router.get("/new", (req, res) => {
+      res.render("new-listing");
+  });
+
+  // add new listing
+  router.post("/", (req,res) => {
+    let queryString = `
+    INSERT INTO items(seller_id, title, price, description, photo_url)
+    VALUES($1, $2, $3, $4, $5)
+    RETURNING *;
+    `
+    let queryParams = [req.session.user_id, req.body.title, req.body.price, req.body.description, req.body.photo_url]
+    db.query(queryString,queryParams)
+    .then(data => {
+      const listings = data.rows;
+      console.log(listings)
+      res.redirect('/api/mylistings');
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
+  })
+
   return router;
 };
