97e5679afdf897145945ab16ea8cc7e9eff3236c
b1u3too <almadsen792@live.ca>
2022-05-02 19:57:49 +0000

    add setval for serial id to pick up after seed data - tidied formatting during testing

--- db/seeds/04_messages.sql
+++ db/seeds/04_messages.sql
@@ -1,6 +1,7 @@
 INSERT INTO messages (id, buyer_id, item_id, created_at, body)
-VALUES (1, 5, 1, '2022-04-01 10:30:30', 'Hello I want to buy this lighthouse');
-INSERT INTO messages (id, buyer_id, item_id, created_at, body)
-VALUES (2, 1, 6, '2022-04-10 11:30:00', 'I will pay $10 for it');
-INSERT INTO messages (id, buyer_id, item_id, created_at, body)
-VALUES (3, 5, 7, '2022-04-11 20:30:00', 'Thank you!');
+VALUES
+  (1, 5, 1, '2022-04-01 10:30:30', 'Hello I want to buy this lighthouse'),
+  (2, 1, 6, '2022-04-10 11:30:00', 'I will pay $10 for it'),
+  (3, 5, 7, '2022-04-11 20:30:00', 'Thank you!');
+
+SELECT setval('messages_id_seq', 3);
