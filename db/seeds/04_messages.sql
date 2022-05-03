125344b9a7b176afd925af7a6ff7f8e2ed301e97
Kazuko Baynton <kazukobaynton@gmail.com>
2022-05-02 19:22:23 -0700

    still trying to figure it out...

--- db/seeds/04_messages.sql
+++ db/seeds/04_messages.sql
@@ -1,14 +1,14 @@
-INSERT INTO messages (id, user_id, from_buyer, item_id, created_at, body)
-VALUES (1, 5, TRUE, 1, '2022-04-01 10:30:30', 'Hello I want to buy this lighthouse');
-INSERT INTO messages (id, user_id, from_buyer, item_id, created_at, body)
-VALUES (2, 1, TRUE, 6, '2022-04-10 11:30:00', 'I will pay $10 for it');
-INSERT INTO messages (id, user_id, from_buyer, item_id, created_at, body)
-VALUES (3, 5, TRUE, 7, '2022-04-11 20:30:00', 'Thank you!');
-INSERT INTO messages (id, user_id, from_buyer, item_id, created_at, body)
-VALUES (4, 1, FALSE, 1, '2022-04-12 10:30:00', 'Sorry for my late reply Buyer 5! Still interested?');
-INSERT INTO messages (id, user_id, from_buyer, item_id, created_at, body)
-VALUES (5, 5, TRUE, 1, '2022-04-12 10:35:00', 'Yes I am still interested!');
-INSERT INTO messages (id, user_id, from_buyer, item_id, created_at, body)
-VALUES (6, 2, TRUE, 1, '2022-04-12 11:30:00', 'Hey! I wanna buy that lighthouse! Thanks! - Buyer 2');
-INSERT INTO messages (id, user_id, from_buyer, item_id, created_at, body)
-VALUES (7, 1, FALSE, 1, '2022-04-12 11:50:00', 'Sorry someone else is interested in it.');
+INSERT INTO messages (id, sender_id, receiver_id, item_id, created_at, body)
+VALUES (1, 5, 1, 1, '2022-04-01 10:30:30', 'Hello I want to buy this lighthouse');
+INSERT INTO messages (id, sender_id, receiver_id, item_id, created_at, body)
+VALUES (2, 1, 3, 6, '2022-04-10 11:30:00', 'I will pay $10 for it');
+INSERT INTO messages (id, sender_id, receiver_id, item_id, created_at, body)
+VALUES (3, 5, 3, 7, '2022-04-11 20:30:00', 'Thank you!');
+INSERT INTO messages (id, sender_id, receiver_id, item_id, created_at, body)
+VALUES (4, 1, 5, 1, '2022-04-12 10:30:00', 'Sorry for my late reply Buyer 5! Still interested?');
+INSERT INTO messages (id, sender_id, receiver_id, item_id, created_at, body)
+VALUES (5, 5, 1, 1, '2022-04-12 10:35:00', 'Yes I am still interested!');
+INSERT INTO messages (id, sender_id, receiver_id, item_id, created_at, body)
+VALUES (6, 2, 1, 1, '2022-04-12 11:30:00', 'Hey! I wanna buy that lighthouse! Thanks! - Buyer 2');
+INSERT INTO messages (id, sender_id, receiver_id, item_id, created_at, body)
+VALUES (7, 1, 2, 1, '2022-04-12 11:50:00', 'Sorry someone else is interested in it.');
--- routes/messages.js
+++ routes/messages.js
@@ -1,6 +1,42 @@
 const express = require('express');
 const router  = express.Router();
 
+const getNameFromID = function(userId, db) {
+
+const queryString = `SELECT users.name FROM users
+    WHERE id = $1 `;
+const queryParams = [userId];
+
+db.query(queryString, queryParams)
+  .then(name => {
+    return name;
+  })
+  .catch(err => {
+    res
+      .status(500)
+      .json({ error: err.message });
+  });
+}
+
+const getConversationPartners = function(userId, arrayOfMessages, db) {
+  const people = [];
+
+  for (const message of arrayOfMessages) {
+    const sendId = message.sender_id;
+    const receiveId = message.receiver_id;
+
+    if (sendId !== userId && !people.includes(sendId)) {
+      people.push({ id: sendId, name: getNameFromID(sendId, db) });
+    }
+
+    if (receiveId !== userId && !people.includes(receiveId)) {
+      people.push(receiveId);
+    }
+  }
+
+  return people;
+}
+
 module.exports = (db) => {
 
   // show all the seller's message(logged in as a seller?)
@@ -10,14 +46,21 @@ module.exports = (db) => {
     SELECT messages.*, users.name AS placeholder_name
     FROM messages
     JOIN items ON item_id = items.id
-    JOIN users ON user_id = users.id
-    WHERE items.seller_id = $1
+    JOIN users ON sender_id = users.id
+    WHERE messages.sender_id = $1 OR
+    messages.receiver_id = $1
+    GROUP BY messages.id, users.name, messages.sender_id, messages.receiver_id
     `;
 
     db.query(queryString,[req.session.user_id])
       .then(data => {
         const messages = data.rows;
-        res.render("mymessages", {messages: messages});
+        console.log(messages);
+        //SCIENCE TOWN
+        const messageFeeds = getConversationPartners(req.session.user_id, messages, db);
+        console.log(messageFeeds);
+        //END OF SCIENCE TOWN
+        res.render("mymessages", { messageFeeds });
       })
       .catch(err => {
         res
@@ -27,18 +70,14 @@ module.exports = (db) => {
   });
 
   // showing all the buyer's messages(loggein in as a seller)
-  router.get("/:buyerId/buyer", (req, res) => {
-
+  router.get("/:buyerId/", (req, res) => {
+    console.log(req.params.id);
     let queryString = `
-     SELECT messages.*, users.name AS placeholder_name
-     FROM messages
-     JOIN items ON item_id = items.id
-     JOIN users ON user_id = users.id
-     WHERE items.seller_id = $1
-     AND messages.user_id IN ($1, $2)
+    SELECT * FROM messages
+    WHERE sender_id = (SELECT sender_id FROM messages WHERE sender_id = $1 ORDER BY created_at DESC FETCH FIRST ROW ONLY) AND receiver_id = (SELECT receiver_id FROM messages WHERE sender_id = $1 ORDER BY created_at DESC FETCH FIRST ROW ONLY) OR sender_id = (SELECT receiver_id FROM messages WHERE sender_id = $1 ORDER BY created_at DESC FETCH FIRST ROW ONLY) AND receiver_id = (SELECT sender_id FROM messages WHERE sender_id = $1 ORDER BY created_at DESC FETCH FIRST ROW ONLY);
      `;
 
-     const queryParams = [req.session.user_id, req.params.buyerId]
+     const queryParams = [req.params.buyerId]
 
      db.query(queryString, queryParams)
        .then(data => {
@@ -52,31 +91,31 @@ module.exports = (db) => {
        });
    });
 
-  // showing all the buyer's messages (when logged in as buyer?)
-  router.get("/:sellerId/seller", (req, res) => {
+  // // showing all the buyer's messages (when logged in as buyer?)
+  // router.get("/:sellerId/seller", (req, res) => {
 
-    let queryString = `
-     SELECT messages.*, users.name AS placeholder_name
-     FROM messages
-     JOIN items ON item_id = items.id
-     JOIN users ON user_id = users.id
-     WHERE items.seller_id = $2
-     AND messages.user_id IN ($1, $2)
-     `;
+  //   let queryString = `
+  //    SELECT messages.*, users.name AS placeholder_name
+  //    FROM messages
+  //    JOIN items ON item_id = items.id
+  //    JOIN users ON user_id = users.id
+  //    WHERE items.seller_id = $2
+  //    AND messages.user_id IN ($1, $2)
+  //    `;
 
-     const queryParams = [req.session.user_id, req.params.sellerId]
+  //    const queryParams = [req.session.user_id, req.params.sellerId]
 
-     db.query(queryString, queryParams)
-       .then(data => {
-         const messages = data.rows;
-         res.render("mymessages", {messages: messages});
-       })
-       .catch(err => {
-         res
-           .status(500)
-           .json({ error: err.message });
-       });
-   });
+  //    db.query(queryString, queryParams)
+  //      .then(data => {
+  //        const messages = data.rows;
+  //        res.render("mymessages", {messages: messages});
+  //      })
+  //      .catch(err => {
+  //        res
+  //          .status(500)
+  //          .json({ error: err.message });
+  //      });
+  //  });
 
   // add new message
   router.post("/", (req, res) => {
